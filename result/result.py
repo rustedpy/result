47e16d463287ae3d06f2a6b418e669bf97f56a1e
Danilo Bargen <mail@dbrgn.ch>
2017-07-12 23:10:45 +0200

    Switch to Python 3 function annotations

--- result/result.py
+++ result/result.py
@@ -13,8 +13,7 @@ class Result(Generic[E, T]):
     Not all methods (https://doc.rust-lang.org/std/result/enum.Result.html)
     have been implemented, only the ones that make sense in the Python context.
     """
-    def __init__(self, is_ok, value, force=False):
-        # type: (bool, Union[E, T], bool) -> None
+    def __init__(self, is_ok: bool, value: Union[E, T], force: bool = False) -> None:
         """Do not call this constructor, use the Ok or Err class methods instead.
 
         There are no type guarantees on the value if this is called directly.
@@ -32,22 +31,18 @@ class Result(Generic[E, T]):
             self._is_ok = is_ok
             self._value = value
 
-    def __eq__(self, other):
-        # type: (Any) -> bool
+    def __eq__(self, other: Any) -> bool:
         return (self.__class__ == other.__class__ and
                 self.is_ok() == cast(Result, other).is_ok() and
                 self._value == other._value)
 
-    def __ne__(self, other):
-        # type: (Any) -> bool
+    def __ne__(self, other: Any) -> bool:
         return not (self == other)
 
-    def __hash__(self):
-        # type: () -> int
+    def __hash__(self) -> int:
         return hash((self.is_ok(), self._value))
 
-    def __repr__(self):
-        # type: () -> str
+    def __repr__(self) -> str:
         if self.is_ok():
             return 'Ok({})'.format(repr(self._value))
         else:
@@ -55,51 +50,42 @@ class Result(Generic[E, T]):
 
     @classmethod
     @overload
-    def Ok(cls):
-        # type: () -> Result[E, bool]
+    def Ok(cls) -> 'Result[E, bool]':
         pass
 
     @classmethod
     @overload
-    def Ok(cls, value):
-        # type: (T) -> Result[E, T]
+    def Ok(cls, value: T) -> 'Result[E, T]':
         pass
 
     @classmethod
-    def Ok(cls, value=True):
-        # type: (Any) -> Result[E, Any]
+    def Ok(cls, value: Any = True) -> 'Result[E, Any]':
         return cls(is_ok=True, value=value, force=True)
 
     @classmethod
-    def Err(cls, error):
-        # type: (E) -> Result[E, T]
+    def Err(cls, error: E) -> 'Result[E, T]':
         return cls(is_ok=False, value=error, force=True)
 
-    def is_ok(self):
-        # type: () -> bool
+    def is_ok(self) -> bool:
         return self._is_ok
 
-    def is_err(self):
-        # type: () -> bool
+    def is_err(self) -> bool:
         return not self._is_ok
 
-    def ok(self):
-        # type: () -> Optional[T]
+    def ok(self) -> Optional[T]:
         """
         Return the value if it is an `Ok` type. Return `None` if it is an `Err`.
         """
         return cast(T, self._value) if self.is_ok() else None
 
-    def err(self):
-        # type: () -> Optional[E]
+    def err(self) -> Optional[E]:
         """
         Return the error if this is an `Err` type. Return `None` otherwise.
         """
         return cast(E, self._value) if self.is_err() else None
 
     @property
-    def value(self):
-        # type: () -> Union[E, T]
+    def value(self) -> Union[E, T]:
         """
         Return the inner value. This might be either the ok or the error type.
         """
@@ -109,27 +95,23 @@ class Result(Generic[E, T]):
 
 
 @overload
-def Ok():
-    # type: () -> Result[E, bool]
+def Ok() -> Result[E, bool]:
     pass
 
 
 @overload
-def Ok(value):
-    # type: (T) -> Result[E, T]
+def Ok(value: T) -> Result[E, T]:
     pass
 
 
-def Ok(value=True):
-    # type: (Any) -> Result[E, Any]
+def Ok(value: Any = True) -> Result[E, Any]:
     """
     Shortcut function to create a new Result.
     """
     return Result.Ok(value)
 
 
-def Err(error):
-    # type: (E) -> Result[E, T]
+def Err(error: E) -> Result[E, T]:
     """
     Shortcut function to create a new Result.
     """
