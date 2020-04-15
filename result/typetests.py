36434bba5857a57ad87327d86c26801790dcde83
Jason van den Hurk <jasonhurk@gmail.com>
2020-04-15 10:41:21 +0200

    Narrow down types in the typetests

--- result/typetests.py
+++ result/typetests.py
@@ -5,10 +5,12 @@ from .result import Result, Ok, Err
 
 res1 = Ok('hello')  # type: Result[str, int]
 if isinstance(res1, Ok):
-    ok = res1.ok()  # type: Optional[str]
+    ok = res1  # type: Ok[str]
+    okValue = res1.ok()  # type: str
     mapped_to_float = res1.map_or(1.0, lambda s: len(s) * 1.5)  # type: float
 else:
-    err = res1.err()  # type: Optional[int]
+    err = res1  # type: Err[int]
+    errValue = err.err()  # type: int
     mapped_to_list = res1.map_err(lambda e: [e]).err()  # type: Optional[List[int]]
 
 # Test constructor functions
