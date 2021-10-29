c6dda2ae623f0bed50fa2d0afe0512c896170a2e
wouter bolsterlee <wouter@bolsterl.ee>
2021-10-29 17:14:34 +0000

    Migrate to a modern (mostly) declarative setuptools config (#63)
    This migrates the packaging infra to a modern mostly declarative
    setuptools configuration, almost literally following the best practices
    layed out in the setuptools documentation; see https://setuptools.pypa.io/
    
    The version info has moved to a ‘result.__version__’ attribute (in the
    ‘__init__.py’ file), which is a common pattern in many packages, and
    setuptools reads it from there (single source of truth). Run black+isort
    on the file while at it.
    
    The new packages can be built the modern way using the PEP517 compliant
    ‘build’ tool via ‘python -m build’ after a ‘pip install build’. Reflect
    this in RELEASING.md. The remaining setup.py file is almost empty, but
    it is not removed because it is still needed for --editable development
    installs. See setuptools docs and this article for details:
    https://blog.ganssle.io/articles/2021/10/setup-py-deprecated.html
    
    Also added a packaging build step to the CI to ensure packaging keeps working.

--- setup.py
+++ setup.py
@@ -1,28 +1,3 @@
 from setuptools import setup
 
-readme = open('README.rst').read()
-
-setup(name='result',
-      version='0.7.0-dev',
-      description='A rust-like result type for Python',
-      author='Danilo Bargen',
-      author_email='mail@dbrgn.ch',
-      url='https://github.com/rustedpy/result',
-      packages=['result'],
-      package_data={'result': ['py.typed']},
-      zip_safe=True,
-      include_package_data=True,
-      license='MIT',
-      keywords='rust result enum',
-      long_description=readme,
-      classifiers=[
-          'Development Status :: 4 - Beta',
-          'License :: OSI Approved :: MIT License',
-          'Programming Language :: Python :: 3',
-          'Programming Language :: Python :: 3.6',
-          'Programming Language :: Python :: 3.7',
-          'Programming Language :: Python :: 3.8',
-          'Programming Language :: Python :: 3.9',
-          'Programming Language :: Python :: 3.10',
-          'Programming Language :: Python :: 3 :: Only',
-      ])
+setup()
